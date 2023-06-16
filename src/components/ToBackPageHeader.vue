461ddaaa7abec0766ee16849c17f19477d9d75dd
zaidsmd <zaidsmd111@gmail.com>
2023-06-16 23:52:05 +0100

    page header with back button component

---
+++ src/components/ToBackPageHeader.vue
@@ -0,0 +1,33 @@
+<script setup>
+import IconBack from "@/components/icons/IconBack.vue";
+defineProps({
+  text:String
+})
+</script>
+
+<template>
+  <div>
+    <IconBack class="back-icon"  @click="$router.go(-1)" />
+    <h1>{{text}}</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: .5rem;
+  width: 100%;
+  color: var(--black-color);
+  margin-bottom: 1rem;
+}
+.back-icon {
+  width: 2rem;
+}
+
+h1 {
+  font-size: 1.7rem;
+  font-weight: 600;
+}
+</style>
