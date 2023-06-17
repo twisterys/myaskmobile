ed50280d4a72569ea4ba645ebfef6cad9da7610b
zaidsmd <zaidsmd111@gmail.com>
2023-06-17 03:17:35 +0100

    CustomCheckBox

---
+++ src/components/CustomCheckBox.vue
@@ -0,0 +1,64 @@
+<script setup>
+import tab from "bootstrap/js/src/tab";
+import {checkbox} from "ionicons/icons";
+
+const props =defineProps({
+  label: String,
+  modelValue:String,
+  name:String,
+  disable:Boolean,
+  checked:Boolean,
+})
+
+const clickHandler = e => {
+  if (!props.disable){
+    e.target.firstChild.checked = !e.target.firstChild.checked;
+    e.target.children[1].classList.toggle('active');
+  }
+}
+</script>
+
+<template>
+  <div @click="clickHandler" class="ask-checkbox">
+    <input @change=" disable ? $emit('input', $event.target.checked) :''"  :checked="checked"  type="checkbox" :value="modelValue"  :name="name">
+    <span :class="checked? 'active':null" ></span>
+    <p>{{ label }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.ask-checkbox {
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+  padding: .3rem 1rem;
+  box-shadow:var(--global-shadow) ;
+  width: fit-content;
+  border-radius: var(--full-border-radius);
+  *{
+    pointer-events: none;
+  }
+  span {
+    background-color: var(--gray-color-200);
+    border-radius: var(--full-border-radius);
+    width: 14px;
+    height: 14px;
+    position: relative;
+    &.active {
+      background-color: var(--blue-color-400);
+      &::after {
+        content: "";
+        position: absolute;
+        top:calc(50% - 2px);
+        left: calc(50% - 2px);
+        border: 2px solid white ;
+        border-radius: var(--full-border-radius);
+      }
+    }
+  }
+}
+
+input {
+  display: none;
+}
+</style>
