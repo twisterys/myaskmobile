121aee798efd9371f0890438c3d7031f0ea4e2da
Yassir Samadi <yassirsamadi@gmail.com>
2023-06-20 02:10:41 +0100

    some change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ma.askgs.mobile',
+  appId: 'ma.asksgs.mobile',
   appName: 'MyAsk',
   webDir: 'dist',
   server: {
